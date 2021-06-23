33ecaeee681b58c7e9d790291f3d9aefac3e94fc
StollToaldo <gs.toaldo@gmail.com>
2021-06-23 16:34:12 -0300

    create Loading component

---
+++ components/Loading.js
@@ -0,0 +1,17 @@
+const Loading = () => {
+  return (
+    <aside
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        padding: 32,
+        backgroundColor: 'blue',
+      }}
+    >
+      Por favor, aguarde...
+    </aside>
+  );
+};
+
+export default Loading;
