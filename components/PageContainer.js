2c0a2a95b52acae51569c740011df5e01697747c
StollToaldo <gs.toaldo@gmail.com>
2021-06-25 12:18:40 -0300

    configure styled components

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -4,6 +4,9 @@ const PageContainer = ({ children }) => <Container>{children}</Container>;
 
 const Container = styled.div`
   height: 100%;
+  max-width: 1200px;
+  margin: auto;
+  padding: 0 16px;
 `;
 
 export default PageContainer;
